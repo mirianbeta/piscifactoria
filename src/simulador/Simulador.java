1ebf241733fe1d8d1c9324b1320aa70239d44a6e
mirianbeta <miyibeta1413@gmail.com>
2023-10-23 15:32:19 +0200

    Segundo commit

--- src/simulador/Simulador.java
+++ src/simulador/Simulador.java
@@ -1,5 +1,26 @@
 package simulador;
 
 public class Simulador {
+
+    int dias = 0;
+
+
+    public void menu(){
+        System.out.println("Menú");
+        System.out.println("1. Estado general");
+        System.out.println("2. Estado piscifactoría");
+        System.out.println("3. Estado tanques");
+        System.out.println("4. Informes");
+        System.out.println("5. Ictiopedia");
+        System.out.println("6. Pasar día");
+        System.out.println("7. Comprar comida");
+        System.out.println("8. Comprar peces");
+        System.out.println("9. Vender peces");
+        System.out.println("10. Limpiar tanques");
+        System.out.println("11. Vaciar tanque");
+        System.out.println("12. Mejorar");
+        System.out.println("13. Pasar varios días");
+        System.out.println("14. Salir");
+    }
     
 }
